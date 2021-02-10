1e9e16535a0f045dd9f42624e1e98f1a4f5d2dcd
OlegMikus <mikus.oleg@gmail.com>
2021-02-10 21:38:04 +0300

    prettier code

--- Blog_one/users/views.py
+++ Blog_one/users/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
