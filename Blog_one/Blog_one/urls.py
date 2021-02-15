fdf6438b537c027ae36aad2f3b54456ef621dec1
OlegMikus <mikus.oleg@gmail.com>
2021-02-15 23:30:52 +0300

    new url to all users posts for anon user

--- Blog_one/Blog_one/urls.py
+++ Blog_one/Blog_one/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import PostsUserListView
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 
+from users.views import ProfileListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile_settings/', user_views.profile_settings, name='profile_settings'),
-    path('profile/', user_views.profile, name='profile'),
+    path('profile/', ProfileListView.as_view(), name='profile'),
+    path('profile/<id>/', PostsUserListView.as_view(), name='profile-public'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
