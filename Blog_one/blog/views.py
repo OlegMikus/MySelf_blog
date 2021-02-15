27cb5fd7bbc8f4a1ebba93c5051bc5f5faf131f9
OlegMikus <mikus.oleg@gmail.com>
2021-02-15 23:35:02 +0300

    add view to all users posts for anon user

--- Blog_one/blog/views.py
+++ Blog_one/blog/views.py
@@ -4,6 +4,13 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
+class PostsUserListView(ListView):
+    model = Post
+    template_name = 'blog/posts_from_user.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
