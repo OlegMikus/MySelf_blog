2768d8e7d568bcdedb8c973afdcd3db4e07ea0c1
OlegMikus <mikus.oleg@gmail.com>
2021-02-20 00:57:23 +0300

    update PostsListView

--- Blog_one/blog/views.py
+++ Blog_one/blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -9,6 +8,12 @@ class PostsUserListView(ListView):
     template_name = 'blog/posts_from_user.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+    def get_queryset(self):
+        context = super().get_queryset()
+
+        post_id = self.request.path_info.split('/')[2]
+        return context.filter(author__profile__id=post_id)
 
 
 class PostListView(ListView):
@@ -55,7 +60,3 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
-
-
-def about(request):
-    return render(request, 'blog/about.html', {'title': 'About'})
