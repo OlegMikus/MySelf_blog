ebdc4a3c36ec24599050eee52dfb517f6fccb2fa
OlegMikus <mikus.oleg@gmail.com>
2021-02-22 14:11:43 +0300

    update views for pagination

--- Blog_one/blog/views.py
+++ Blog_one/blog/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -7,13 +10,12 @@ class PostsUserListView(ListView):
     model = Post
     template_name = 'blog/posts_from_user.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+
+    paginate_by = 5
 
     def get_queryset(self):
-        context = super().get_queryset()
-
-        post_id = self.request.path_info.split('/')[2]
-        return context.filter(author__profile__id=post_id)
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostListView(ListView):
@@ -21,6 +23,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
