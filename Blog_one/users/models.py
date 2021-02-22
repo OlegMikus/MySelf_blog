7e7c9e4590c9f7dd7bc144265e22c23e1b138dd9
OlegMikus <mikus.oleg@gmail.com>
2021-02-22 14:13:18 +0300

    add first_name and last_name fields

--- Blog_one/users/models.py
+++ Blog_one/users/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.urls import reverse
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    first_name = models.CharField(default='name', max_length=50)
+    last_name = models.CharField(default='surname', max_length=50)
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -19,3 +22,6 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+    def get_absolute_url(self):
+        return reverse('profile-public', kwargs={'pk': self.pk})
